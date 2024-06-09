a099f285e8cac8c3a7298237043f40c5f659d1bb
Arturo Villalobos <rturohouse@gmail.com>
2024-06-09 18:21:12 -0500

    some macos specific changes

--- machine_report.sh
+++ machine_report.sh
@@ -5,6 +5,7 @@
 # Basic configuration, change as needed
 report_title="UNITED STATES GRAPHICS COMPANY"
 zfs_filesystem="zroot/ROOT/os"
+# -f /sbin/apfs_hfs_convert
 
 # Utilities
 bar_graph() {
@@ -27,61 +28,77 @@ bar_graph() {
 }
 
 # Operating System Information
-source /etc/os-release
-os_name="${ID^} $(cat /etc/debian_version) ${VERSION_CODENAME^}"
-os_kernel=$({ uname; uname -r; } | tr '\n' ' ')
+if [ "$(uname)" == "Linux" ]; then
+    source /etc/os-release
+    os_name="${ID^} $(cat /etc/debian_version) ${VERSION_CODENAME^}"
+    os_kernel=$({ uname; uname -r; } | tr '\n' ' ')
+elif [ "$(uname)" == "Darwin" ]; then # MACOS specific
+    ID=$(sw_vers | grep 'ProductName' | cut -d: -f2 | xargs)
+    VERSION_CODE=$(sw_vers | grep 'ProductVersion' | cut -d: -f2 | xargs)
+    # Couldnt for the life of me find a plist file where sonoma is mentions so for now its hardcoded
+    os_name="${ID} ${VERSION_CODE} Sonoma"
+    os_kernel=$({ uname; uname -r; uname -m; } | tr '\n' ' ')
+fi
 
 # Network Information
-net_current_user=$(whoami)
-net_hostname=$(hostname -f)
-net_machine_ip=$(hostname -I)
-net_client_ip=$(who am i --ips | awk '{print $5}')
-net_dns_ip=$(grep 'nameserver' /etc/resolv.conf | awk '{print $2}')
+if [ "$(uname)" == "Linux" ]; then
+    net_current_user=$(whoami)
+    net_hostname=$(hostname -f)
+    net_machine_ip=$(hostname -I)
+    net_client_ip=$(who am i --ips | awk '{print $5}')
+    net_dns_ip=$(grep 'nameserver' /etc/resolv.conf | awk '{print $2}')
+elif [ "$(uname)" == "Darwin" ]; then # MACOS specific
+    net_current_user=$(whoami)
+    net_hostname=$(hostname -f)
+    net_machine_ip=$(ipconfig getifaddr en0)
+    net_client_ip=$(who -m | awk '{print $5}')
+    net_dns_ip=$(grep 'nameserver' /etc/resolv.conf | awk '{print $2}')
+fi
 
-# CPU Information
-cpu_model="$(lscpu | grep 'Model name' | grep -v 'BIOS' | cut -f 2 -d ':' | awk '{print $1 " "  $2 " " $3}')"
-cpu_hypervisor="$(lscpu | grep 'Hypervisor vendor' | cut -f 2 -d ':' | awk '{$1=$1}1')"
-cpu_cores="$(nproc --all)"
-cpu_cores_per_socket="$(lscpu | grep 'Core(s) per socket' | cut -f 2 -d ':'| awk '{$1=$1}1')"
-cpu_sockets="$(lscpu | grep 'Socket(s)' | cut -f 2 -d ':' | awk '{$1=$1}1')"
-cpu_freq="$(grep 'cpu MHz' /proc/cpuinfo | cut -f 2 -d ':' | awk 'NR==1' | awk '{$1=$1}1' | numfmt --from-unit=M --to-unit=G --format %.2f)"
+# # CPU Information
+# cpu_model="$(lscpu | grep 'Model name' | grep -v 'BIOS' | cut -f 2 -d ':' | awk '{print $1 " "  $2 " " $3}')"
+# cpu_hypervisor="$(lscpu | grep 'Hypervisor vendor' | cut -f 2 -d ':' | awk '{$1=$1}1')"
+# cpu_cores="$(nproc --all)"
+# cpu_cores_per_socket="$(lscpu | grep 'Core(s) per socket' | cut -f 2 -d ':'| awk '{$1=$1}1')"
+# cpu_sockets="$(lscpu | grep 'Socket(s)' | cut -f 2 -d ':' | awk '{$1=$1}1')"
+# cpu_freq="$(grep 'cpu MHz' /proc/cpuinfo | cut -f 2 -d ':' | awk 'NR==1' | awk '{$1=$1}1' | numfmt --from-unit=M --to-unit=G --format %.2f)"
 
-load_avg_1min=$(uptime | awk -F'load average: ' '{print $2}' | cut -d ',' -f1 | tr -d ' ')
-load_avg_5min=$(uptime | awk -F'load average: ' '{print $2}' | cut -d ',' -f2 | tr -d ' ')
-load_avg_15min=$(uptime| awk -F'load average: ' '{print $2}' | cut -d ',' -f3 | tr -d ' ')
+# load_avg_1min=$(uptime | awk -F'load average: ' '{print $2}' | cut -d ',' -f1 | tr -d ' ')
+# load_avg_5min=$(uptime | awk -F'load average: ' '{print $2}' | cut -d ',' -f2 | tr -d ' ')
+# load_avg_15min=$(uptime| awk -F'load average: ' '{print $2}' | cut -d ',' -f3 | tr -d ' ')
 
-cpu_1min_bar_graph=$(bar_graph "$load_avg_1min" "$cpu_cores")
-cpu_5min_bar_graph=$(bar_graph "$load_avg_5min" "$cpu_cores")
-cpu_15min_bar_graph=$(bar_graph "$load_avg_15min" "$cpu_cores")
+# cpu_1min_bar_graph=$(bar_graph "$load_avg_1min" "$cpu_cores")
+# cpu_5min_bar_graph=$(bar_graph "$load_avg_5min" "$cpu_cores")
+# cpu_15min_bar_graph=$(bar_graph "$load_avg_15min" "$cpu_cores")
 
-# Memory Information
-mem_total=$(grep 'MemTotal' /proc/meminfo | awk '{print $2}')
-mem_available=$(grep 'MemAvailable' /proc/meminfo | awk '{print $2}')
-mem_used=$((mem_total - mem_available))
-mem_percent=$(echo "$mem_used / $mem_total * 100" | bc -l)
-mem_percent=$(printf "%.2f" "$mem_percent")
-mem_total_gb=$(echo "$mem_total" | numfmt --from-unit=Ki --to-unit=Gi --format %.2f)
-mem_available_gb=$(echo "$mem_available" | numfmt --from-unit=Ki --to-unit=Gi --format %.2f) # Not used currently
-mem_used_gb=$(echo "$mem_used" | numfmt  --from-unit=Ki --to-unit=Gi --format %.2f)
-mem_bar_graph=$(bar_graph "$mem_used" "$mem_total")
+# # Memory Information
+# mem_total=$(grep 'MemTotal' /proc/meminfo | awk '{print $2}')
+# mem_available=$(grep 'MemAvailable' /proc/meminfo | awk '{print $2}')
+# mem_used=$((mem_total - mem_available))
+# mem_percent=$(echo "$mem_used / $mem_total * 100" | bc -l)
+# mem_percent=$(printf "%.2f" "$mem_percent")
+# mem_total_gb=$(echo "$mem_total" | numfmt --from-unit=Ki --to-unit=Gi --format %.2f)
+# mem_available_gb=$(echo "$mem_available" | numfmt --from-unit=Ki --to-unit=Gi --format %.2f) # Not used currently
+# mem_used_gb=$(echo "$mem_used" | numfmt  --from-unit=Ki --to-unit=Gi --format %.2f)
+# mem_bar_graph=$(bar_graph "$mem_used" "$mem_total")
 
-# Disk Information
-# TODO: Add checks if zfs file system exists
-# WARNING: This script assumes that the zfs file system is located at $zfs_filesystem
-zfs_health=$(zpool status -x zroot | grep -q "is healthy" && echo  "HEALTH O.K.")
-zfs_available=$( zfs get -o value -Hp available "$zfs_filesystem")
-zfs_used=$( zfs get -o value -Hp used "$zfs_filesystem")
-zfs_available_gb=$(echo "$zfs_available" | numfmt --to-unit=G --format %.2f)
-zfs_used_gb=$(echo "$zfs_used" | numfmt --to-unit=G --format %.2f)
-disk_percent=$(printf "%.2f" "$(echo "$zfs_used / $zfs_available * 100" | bc -l)")
-disk_bar_graph=$(bar_graph "$zfs_used" "$zfs_available")
+# # Disk Information
+# # TODO: Add checks if zfs file system exists
+# # WARNING: This script assumes that the zfs file system is located at $zfs_filesystem
+# zfs_health=$(zpool status -x zroot | grep -q "is healthy" && echo  "HEALTH O.K.")
+# zfs_available=$( zfs get -o value -Hp available "$zfs_filesystem")
+# zfs_used=$( zfs get -o value -Hp used "$zfs_filesystem")
+# zfs_available_gb=$(echo "$zfs_available" | numfmt --to-unit=G --format %.2f)
+# zfs_used_gb=$(echo "$zfs_used" | numfmt --to-unit=G --format %.2f)
+# disk_percent=$(printf "%.2f" "$(echo "$zfs_used / $zfs_available * 100" | bc -l)")
+# disk_bar_graph=$(bar_graph "$zfs_used" "$zfs_available")
 
-# Last login and Uptime
-last_login=$(lastlog -u root)
-last_login_ip=$(echo "$last_login" | awk 'NR==2 {print $3}')
-last_login_time=$(echo "$last_login" | awk 'NR==2 {print $5, $6, $7, $8, $9}')
-last_login_formatted_time=$(date -d "$last_login_time" "+%b %-d %Y %T")
-sys_uptime=$(uptime -p | sed 's/up\s*//; s/\s*day\(s*\)/d/; s/\s*hour\(s*\)/h/; s/\s*minute\(s*\)/m/')
+# # Last login and Uptime
+# last_login=$(lastlog -u root)
+# last_login_ip=$(echo "$last_login" | awk 'NR==2 {print $3}')
+# last_login_time=$(echo "$last_login" | awk 'NR==2 {print $5, $6, $7, $8, $9}')
+# last_login_formatted_time=$(date -d "$last_login_time" "+%b %-d %Y %T")
+# sys_uptime=$(uptime -p | sed 's/up\s*//; s/\s*day\(s*\)/d/; s/\s*hour\(s*\)/h/; s/\s*minute\(s*\)/m/')
 
 # Machine Report
 printf "┌┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┬┐\n"
@@ -97,23 +114,23 @@ printf "│ %-10s │ %-29s │\n" "MACHINE IP" "$net_machine_ip"
 printf "│ %-10s │ %-29s │\n" "CLIENT  IP" "$net_client_ip"
 printf "│ %-10s │ %-29s │\n" "DNS     IP" "$net_dns_ip"
 printf "│ %-10s │ %-29s │\n" "USER" "$net_current_user"
-printf "├────────────┼───────────────────────────────┤\n"
-printf "│ %-10s │ %-29s │\n" "PROCESSOR" "$cpu_model"
-printf "│ %-10s │ %-29s │\n" "CORES" "$cpu_cores_per_socket vCPU(s) / $cpu_sockets Socket(s)"
-printf "│ %-10s │ %-29s │\n" "HYPERVISOR" "$cpu_hypervisor"
-printf "│ %-10s │ %-29s │\n" "CPU FREQ" "$cpu_freq GHz"
-printf "│ %-10s │ %-29s │\n" "LOAD  1m" "$cpu_1min_bar_graph"
-printf "│ %-10s │ %-29s │\n" "LOAD  5m" "$cpu_5min_bar_graph"
-printf "│ %-10s │ %-29s │\n" "LOAD 15m" "$cpu_15min_bar_graph"
-printf "├────────────┼───────────────────────────────┤\n"
-printf "│ %-10s │ %-29s │\n" "VOLUME" "$zfs_used_gb/$zfs_available_gb GB [$disk_percent%]"
-printf "│ %-10s │ %-29s │\n" "DISK USAGE" "$disk_bar_graph"
-printf "│ %-10s │ %-29s │\n" "ZFS HEALTH" "$zfs_health"
-printf "├────────────┼───────────────────────────────┤\n"
-printf "│ %-10s │ %-29s │\n" "MEMORY" "${mem_used_gb}/${mem_total_gb} GiB [${mem_percent}%]"
-printf "│ %-10s │ %-29s │\n" "USAGE" "${mem_bar_graph}"
-printf "├────────────┼───────────────────────────────┤\n"
-printf "│ %-10s │ %-29s │\n" "LAST LOGIN" "$last_login_formatted_time"
-printf "│ %-10s │ %-29s │\n" "" "$last_login_ip"
-printf "│ %-10s │ %-29s │\n" "UPTIME" "$sys_uptime"
-printf "└────────────┴───────────────────────────────┘\n"
+# printf "├────────────┼───────────────────────────────┤\n"
+# printf "│ %-10s │ %-29s │\n" "PROCESSOR" "$cpu_model"
+# printf "│ %-10s │ %-29s │\n" "CORES" "$cpu_cores_per_socket vCPU(s) / $cpu_sockets Socket(s)"
+# printf "│ %-10s │ %-29s │\n" "HYPERVISOR" "$cpu_hypervisor"
+# printf "│ %-10s │ %-29s │\n" "CPU FREQ" "$cpu_freq GHz"
+# printf "│ %-10s │ %-29s │\n" "LOAD  1m" "$cpu_1min_bar_graph"
+# printf "│ %-10s │ %-29s │\n" "LOAD  5m" "$cpu_5min_bar_graph"
+# printf "│ %-10s │ %-29s │\n" "LOAD 15m" "$cpu_15min_bar_graph"
+# printf "├────────────┼───────────────────────────────┤\n"
+# printf "│ %-10s │ %-29s │\n" "VOLUME" "$zfs_used_gb/$zfs_available_gb GB [$disk_percent%]"
+# printf "│ %-10s │ %-29s │\n" "DISK USAGE" "$disk_bar_graph"
+# printf "│ %-10s │ %-29s │\n" "ZFS HEALTH" "$zfs_health"
+# printf "├────────────┼───────────────────────────────┤\n"
+# printf "│ %-10s │ %-29s │\n" "MEMORY" "${mem_used_gb}/${mem_total_gb} GiB [${mem_percent}%]"
+# printf "│ %-10s │ %-29s │\n" "USAGE" "${mem_bar_graph}"
+# printf "├────────────┼───────────────────────────────┤\n"
+# printf "│ %-10s │ %-29s │\n" "LAST LOGIN" "$last_login_formatted_time"
+# printf "│ %-10s │ %-29s │\n" "" "$last_login_ip"
+# printf "│ %-10s │ %-29s │\n" "UPTIME" "$sys_uptime"
+# printf "└────────────┴───────────────────────────────┘\n"
